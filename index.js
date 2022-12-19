f7e2e6d07c43c74acd9fd2cda66907e51b022d92
Liza McLain <lizamclain@gmail.com>
2022-12-18 22:17:14 -0600

    commit tree 1e1981c217f95a49d2e5a9612039c2c6925b483d

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(array) {
+    const answer = array.find(array => array.result === "W")
+    console.log(answer)
+    if (answer === undefined) {
+    console.log("didn't work")
+        return undefined;
+    
+     } else {
+        return answer.year
+    }
+}
+
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+
+  
+//   function superbowlWin(record) {
+//     const found = record.find(record => record.result === 'W');
+//     console.log(found);
+//     if (found === undefined) {
+//       return undefined;
+//     } else {
+//       return found.year;
+//     }
+//   }
